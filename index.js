09eee10d7dad1605b9120f43ab4243fdafff30e5
trinitywilliams158 <trinitywilliams158@gmail.com>
2016-11-02 16:07:50 +0000

    Second chillan.

---
+++ index.js
@@ -0,0 +1,31 @@
+var add = function(a,b){
+  return a + b ;
+};
+
+var subtract = function(a,b){
+  return a - b;
+};
+
+var multiply = function(a,b){
+  return a * b;
+};
+
+var divide = function(a,b){
+  return a / b ;
+};
+
+var inc = function(a,b){
+  return a + 1 ;
+};
+
+var dec = function(a,b){
+  return a -1 ;
+};
+
+var makeInt = function(string){
+  return parseInt(string,10)
+};
+
+var preserveDecimal = function(string){
+  return parseFloat(string);
+};
